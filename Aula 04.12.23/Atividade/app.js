127d0f7ce43b2f9a53eaffdb96d09a90b1ea6231
Igorsantos021 <santosigor868@gmail.com>
2023-12-04 22:26:43 -0300

    Site de bandas (iron maiden, Racionais)

---
+++ Aula 04.12.23/Atividade/app.js
@@ -0,0 +1,13 @@
+function jedi(){
+    document.getElementById("mensagem").innerText = "Venha para o lado da luz"
+    document.getElementById("frase").innerText = "Guerras não fazem Ninguem Maior"
+    document.getElementById("imagem").src="jedi.gif"
+    document.body.style="background: #607479"
+}
+
+function sith(){
+    document.getElementById("mensagem").innerText = "Venha para o lado sombrio"
+    document.getElementById("frase_sith"). innerText = "A paz é uma mentira, só a emoção importa"
+    document.getElementById("imagem").src="sith.gif"
+    document.body.style="background: #4f5055"
+}
---
+++ Aula 04.12.23/Atividade02/app.js
@@ -0,0 +1,12 @@
+function racionais(){
+    document.getElementById("mensagem").innerText = "Um pouco sobre o Disco Nada como um Dia após o Outro Dia do Racionais"
+    document.getElementById("disco").innerText = "Nada Como Um Dia Após o Outro Dia é o terceiro álbum de estúdio do grupo brasileiro de rap Racionais MC's, lançado em 27 de outubro de 2002 pela Cosa Nostra"
+    document.getElementById("imagem").src="racionais.jpg"
+}
+
+function iron_maiden(){
+    document.getElementById("mensagem").innerText = "Um pouco sobre o Disco Fear of the Dark do Iron maiden"
+    document.getElementById("disco_iron").innerText = "Fear of the Dark é o nono álbum de estúdio da banda banda inglesa de heavy metal Iron Maiden. Lançado em 11 de maio de 1992, foi o terceiro lançamento de estúdio da banda no topo da parada de álbuns do Reino Unido, e o último a apresentar Bruce Dickinson como vocalista do grupo até seu retorno em 1999.[1] Foi o primeiro disco a ser produzido por Steve Harris, e o último a contar com Martin Birch (que se aposentou depois do lançamento)."
+    document.getElementById("imagem").src="ironmaiden.jpg"
+
+}    
